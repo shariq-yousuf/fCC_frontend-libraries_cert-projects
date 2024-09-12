6877661a1dcc33f090349f0f8ddad8f2da4bd633
shariq-yousuf <shariqyousuf01@gmail.com>
2024-09-12 21:52:21 +0500

    add fCC test cdn and tweet functionality

--- random-quote-machine/src/components/QuoteBox.jsx
+++ random-quote-machine/src/components/QuoteBox.jsx
@@ -6,7 +6,7 @@ const QuoteBox = ({ hadith, getHadithFromAPI, textEl }) => {
   return (
     <div
       id="quote-box"
-      className="min-h-64 md:min-w-2/5 md:max-w-5xl w-11/12 bg-gray-600 px-8 py-4 flex flex-col justify-evenly gap-3"
+      className="min-h-64 md:min-w-2/5 md:max-w-5xl w-11/12 bg-gray-600 text-white px-8 py-4 flex flex-col justify-evenly gap-3"
     >
       <div
         id="text"
@@ -20,11 +20,16 @@ const QuoteBox = ({ hadith, getHadithFromAPI, textEl }) => {
       </div>
       <div id="buttons-container" className="flex justify-between">
         <a
-          href="twitter.com/intent/tweet"
+          href={`https://twitter.com/intent/tweet?hashtags=hadith&text=' +
+      ${encodeURIComponent(
+        hadith.hadithEnglish + "\n - Prophet Muhammad (S.A.W) \n\n"
+      )}`}
+          target="_blank"
+          title="Tweet this Hadith"
           id="tweet-quote"
           className="text-lg bg-red-500 px-4 py-1 hover:bg-red-600"
         >
-          tweet
+          <i class="fa-brands fa-x-twitter"></i>
         </a>
         <button
           id="new-quote"
