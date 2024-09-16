dd22ea4450dee08bd46dbfa72ab209547258b334
shariq-yousuf <shariqyousuf01@gmail.com>
2024-09-16 20:21:38 +0500

    add markeddown parser

--- markdown-previewer/src/App.tsx
+++ markdown-previewer/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react"
 import Editor from "./components/Editor"
 import Previewer from "./components/Previewer"
 
 function App() {
+  const [inputValue, setInputValue] = useState<string>("")
+  const [markedValue, setMarkedValue] = useState("")
+
   return (
     <div className="flex flex-col justify-evenly h-dvh">
       <h1 className="text-5xl font-bold text-center">Markdown Previewer</h1>
       <main className="flex justify-center gap-8">
-        <Editor />
-        <Previewer />
+        <Editor
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          setMarkedValue={setMarkedValue}
+        />
+        <Previewer markedValue={markedValue} />
       </main>
     </div>
   )
--- markdown-previewer/src/components/Editor.tsx
+++ markdown-previewer/src/components/Editor.tsx
@@ -1,10 +1,19 @@
+import { marked } from "marked"
 import { ChangeEvent, useState } from "react"
 
-const Editor = () => {
-  const [inputValue, setInputValue] = useState("")
+interface Props {
+  inputValue: string
+  setInputValue: React.Dispatch<React.SetStateAction<string>>
+  setMarkedValue: React.Dispatch<React.SetStateAction<string>>
+}
 
-  const onInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setInputValue(event.target.value)
+const Editor = ({ inputValue, setInputValue, setMarkedValue }: Props) => {
+  const onInputChange = async (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value
+    setInputValue(value)
+
+    const markedValue = await marked.parse(value)
+    setMarkedValue(markedValue)
   }
 
   return (
