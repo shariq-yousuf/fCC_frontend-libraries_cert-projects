eeb6b61a5efee67854bad239725599df7a7c9810
shariq-yousuf <shariqyousuf01@gmail.com>
2024-09-30 18:58:09 +0500

    change break and session function components to class components

--- pomodoro-timer/src/components/Break.tsx
+++ pomodoro-timer/src/components/Break.tsx
@@ -1,41 +1,98 @@
-import { useState } from "react"
-import { flushSync } from "react-dom"
+import { Component, ReactNode } from "react"
 
-const Break = () => {
-  const [breakLength, setBreakLength] = useState(5)
+class Break extends Component {
+  state: { breakLength: number } = {
+    breakLength: 5,
+  }
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      breakLength: 5,
+    }
+    this.handleDecrement = this.handleDecrement.bind(this)
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
 
-  const handleDecrement = () => {
-    if (breakLength > 1) {
-      flushSync(() => {
-        setBreakLength((prev) => prev - 1)
+  handleDecrement() {
+    if (this.state.breakLength > 1) {
+      this.setState((state: { breakLength: number }) => {
+        return {
+          breakLength: state.breakLength - 1,
+        }
       })
     }
   }
 
-  const handleIncrement = () => {
-    if (breakLength < 60) {
-      flushSync(() => {
-        setBreakLength((prev) => prev + 1)
+  handleIncrement() {
+    if (this.state.breakLength < 60) {
+      this.setState((state: { breakLength: number }) => {
+        return {
+          breakLength: state.breakLength + 1,
+        }
       })
     }
   }
 
-  return (
-    <div id="break-container" className="font-bold text-xl">
-      <h3 id="break-label" className="text-center">
-        Break Length
-      </h3>
-      <div id="btns-container" className="flex justify-center gap-4">
-        <button id="break-decrement" onClick={handleDecrement}>
-          D
-        </button>
-        <p id="break-length">{breakLength}</p>
-        <button id="break-increment" onClick={handleIncrement}>
-          I
-        </button>
+  render(): ReactNode {
+    return (
+      <div id="break-container" className="font-bold text-xl">
+        <h3 id="break-label" className="text-center">
+          Break Length
+        </h3>
+        <div id="btns-container" className="flex justify-center">
+          <button id="break-decrement" className="px-4" onClick={this.handleDecrement}>
+            -
+          </button>
+          <p id="break-length">{this.state.breakLength}</p>
+          <button id="break-increment" className="px-4" onClick={this.handleIncrement}>
+            +
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Break
+
+//  import { useState } from "react"
+// import { flushSync } from "react-dom"
+
+// const Break = () => {
+//   const [breakLength, setBreakLength] = useState(5)
+
+//   const handleDecrement = () => {
+//     if (breakLength > 1) {
+//       flushSync(() => {
+//         setBreakLength((prev) => prev - 1)
+//       })
+//     }
+//   }
+
+//   const handleIncrement = () => {
+//     if (breakLength < 60) {
+//       flushSync(() => {
+//         setBreakLength((prev) => prev + 1)
+//       })
+//     }
+//   }
+
+//   return (
+//     <div id="break-container" className="font-bold text-xl">
+//       <h3 id="break-label" className="text-center">
+//         Break Length
+//       </h3>
+//       <div id="btns-container" className="flex justify-center gap-4">
+//         <button id="break-decrement" onClick={handleDecrement}>
+//           D
+//         </button>
+//         <p id="break-length">{breakLength}</p>
+//         <button id="break-increment" onClick={handleIncrement}>
+//           I
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Break
--- pomodoro-timer/src/components/Session.tsx
+++ pomodoro-timer/src/components/Session.tsx
@@ -1,41 +1,106 @@
-import { useState } from "react"
-import { flushSync } from "react-dom"
+import { Component, ReactNode } from "react"
 
-const Session = () => {
-  const [sessionLength, setSessionLength] = useState(25)
+class Session extends Component {
+  state: { sessionLength: number } = {
+    sessionLength: 25,
+  }
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      sessionLength: 25,
+    }
+    this.handleDecrement = this.handleDecrement.bind(this)
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
 
-  const handleDecrement = () => {
-    if (sessionLength > 1) {
-      flushSync(() => {
-        setSessionLength((prev) => prev - 1)
+  handleDecrement() {
+    if (this.state.sessionLength > 1) {
+      this.setState((state: { sessionLength: number }) => {
+        return {
+          sessionLength: state.sessionLength - 1,
+        }
       })
     }
   }
 
-  const handleIncrement = () => {
-    if (sessionLength < 60) {
-      flushSync(() => {
-        setSessionLength((prev) => prev + 1)
+  handleIncrement() {
+    if (this.state.sessionLength < 60) {
+      this.setState((state: { sessionLength: number }) => {
+        return {
+          sessionLength: state.sessionLength + 1,
+        }
       })
     }
   }
 
-  return (
-    <div id="session-container" className="font-bold text-xl">
-      <h3 id="session-label" className="text-center">
-        Session Length
-      </h3>
-      <div id="btns-container" className="flex justify-center gap-4">
-        <button id="session-decrement" onClick={handleDecrement}>
-          D
-        </button>
-        <p id="session-length">{sessionLength}</p>
-        <button id="session-increment" onClick={handleIncrement}>
-          I
-        </button>
+  render(): ReactNode {
+    return (
+      <div id="session-container" className="font-bold text-xl">
+        <h3 id="session-label" className="text-center">
+          Session Length
+        </h3>
+        <div id="btns-container" className="flex justify-center">
+          <button
+            id="session-decrement"
+            className="px-4"
+            onClick={this.handleDecrement}
+          >
+            -
+          </button>
+          <p id="session-length">{this.state.sessionLength}</p>
+          <button
+            id="session-increment"
+            className="px-4"
+            onClick={this.handleIncrement}
+          >
+            +
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Session
+
+// import { useState } from "react"
+// import { flushSync } from "react-dom"
+
+// const Session = () => {
+//   const [sessionLength, setSessionLength] = useState(25)
+
+//   const handleDecrement = () => {
+//     if (sessionLength > 1) {
+//       flushSync(() => {
+//         setSessionLength((prev) => prev - 1)
+//       })
+//     }
+//   }
+
+//   const handleIncrement = () => {
+//     if (sessionLength < 60) {
+//       flushSync(() => {
+//         setSessionLength((prev) => prev + 1)
+//       })
+//     }
+//   }
+
+//   return (
+//     <div id="session-container" className="font-bold text-xl">
+//       <h3 id="session-label" className="text-center">
+//         Session Length
+//       </h3>
+//       <div id="btns-container" className="flex justify-center gap-4">
+//         <button id="session-decrement" onClick={handleDecrement}>
+//           D
+//         </button>
+//         <p id="session-length">{sessionLength}</p>
+//         <button id="session-increment" onClick={handleIncrement}>
+//           I
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Session
