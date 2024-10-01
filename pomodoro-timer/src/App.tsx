63fc1d49eeccb14dc3eac084aaad1a6a7d94e9f0
shariq-yousuf <shariqyousuf01@gmail.com>
2024-10-01 07:27:53 +0500

    add favicon

--- pomodoro-timer/src/App.tsx
+++ pomodoro-timer/src/App.tsx
@@ -1,6 +1,4 @@
 import { useRef, useState } from "react"
-// import Break from "./components/Break"
-// import Session from "./components/Session"
 import SessionContext, { initialState } from "./context/LengthsContext"
 import Timer from "./components/Timer"
 import Length from "./components/Length"
@@ -35,8 +33,6 @@ function App() {
               Pomodoro Timer
             </h1>
             <div id="lengths-container" className="flex justify-around gap-4">
-              {/* <Break />
-            <Session /> */}
               {lengths.map(({ title, value }) => (
                 <Length key={title} length={value} title={title} />
               ))}
